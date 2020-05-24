a4669b118fc4b8a86eff51b6105fbc76b4cfa8b0
mkdudeja <87manishkumar@gmail.com>
2020-05-24 16:48:41 +0530

    committing changes

--- template.web/src/app/store/reducers/index.ts
+++ template.web/src/app/store/reducers/index.ts
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
+
 import accountLoginReducer from './account-login.reducer';
+
 import { IAppState } from '../../interfaces';
 
 export default combineReducers<IAppState>({
